a4afe675a4b3fd34258b2d152823bda006208e3b
bimo99b9 <danilopezgala@gmail.com>
2022-10-17 23:56:24 +0200

    upload s14

--- S14 - Subajuste, sobreajuste y ruido/S14 - Ejercicios.py
+++ S14 - Subajuste, sobreajuste y ruido/S14 - Ejercicios.py
@@ -1,12 +1,11 @@
 from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 import matplotlib.pyplot as plt
 import pandas as pd
-import pydotplus
 from sklearn import tree
+import pydotplus
 
 print("############################################################################")
 print(
@@ -15,10 +14,10 @@ print(
 print("############################################################################")
 
 df = pd.read_excel(
-    "S14 - Subajuste, sobreajuste y ruido\ejemplo.xlsx", sheet_name="datos"
+    "S14 - Subajuste, sobreajuste y ruido/ejemplo.xlsx", sheet_name="datos"
 )
 df_ruido = pd.read_excel(
-    "S14 - Subajuste, sobreajuste y ruido\ejemplo.xlsx", sheet_name="ruido"
+    "S14 - Subajuste, sobreajuste y ruido/ejemplo.xlsx", sheet_name="ruido"
 )
 df = pd.concat([df, df_ruido], axis=0, ignore_index=True)
 filas, columnas = df.shape
@@ -58,8 +57,9 @@ print("Accuracy Árbol: ", metrics.accuracy_score(y_test, y_pred))
 dot_data = tree.export_graphviz(
     decision_tree=sys_dt, feature_names=X.columns, class_names=["0", "1"], filled=True
 )
-graph = pydotplus.graph_from_dot_data(dot_data)
-graph.write_png("arbol.png")
+
+# graph = pydotplus.graph_from_dot_data(dot_data)
+# graph.write_png("arbol.png")
 
 print("############################################################################")
 print(
@@ -84,7 +84,46 @@ gs_knn = GridSearchCV(
     n_jobs=-1,
 )
 
-gs_tree
+gs_tree = GridSearchCV(
+    sys_dt,
+    param_grid=hyperparameters,
+    scoring="accuracy",
+    cv=folds,
+    verbose=1,
+    n_jobs=-1,
+)
 
 gs_knn_trained = gs_knn.fit(X_train, y_train)
-gs_tree_trained = gs_tree.
+gs_tree_trained = gs_tree.fit(X_train, y_train)
+
+print("KNN - Hyperparams: ", gs_knn_trained.best_params_)
+print("KNN - Mejor accuracy en GridSearch: %.4f" % gs_knn_trained.best_score_)
+print("Árbol - Hyperparams: ", gs_tree_trained.best_params_)
+print("Árbol - Mejor accuracy en GridSearch: %.4f" % gs_tree_trained.best_score_)
+
+print("############################################################################")
+print("5. Evalúa el rendimiento de los sistemas y visualiza el nuevo árbol generado.")
+print("############################################################################")
+
+print("### Resultados ###")
+# Predicciones del KNN
+y_pred_knn = gs_knn_trained.predict(X_train)
+print("KNN - Accuracy en train: %.4f" % metrics.accuracy_score(y_train, y_pred_knn))
+y_pred_knn = gs_knn_trained.predict(X_test)
+print("KNN - Accuracy en test: %.4f" % metrics.accuracy_score(y_test, y_pred_knn))
+
+# Predicciones del árbol
+y_pred_tree = gs_tree_trained.predict(X_train)
+print("Árbol - Accuracy en train: %.4f" % metrics.accuracy_score(y_train, y_pred_tree))
+y_pred_tree = gs_tree_trained.predict(X_test)
+print("Árbol - Accuracy en test: %.4f" % metrics.accuracy_score(y_test, y_pred_tree))
+
+# Visualizar el nuevo arbol
+dot_data = export_graphviz(
+    decision_tree=gs_tree_trained.best_estimator_,
+    feature_names=X.columns,
+    class_names=["0", "1"],
+    filled=True,
+)
+
+graph = pydotplus.graph_from_dot_data(dot_data)
